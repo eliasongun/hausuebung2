0c353b7d0f601483d57b54767092f8316572423c
eliasongun <eongun40@gmail.com>
2020-10-05 09:01:01 +0200

    6. Commit Klasse RationalCalculator

---
+++ eongun18_hausuebung2/src/main/java/htlgrieskirchen/pos/dreia/eongun18/ComplexCalculator.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package htlgrieskirchen.pos.dreia.eongun18;
+
+/**
+ *
+ * @author eongu
+ */
+public class ComplexCalculator extends AbstractCalculator {
+
+    public ComplexCalculator(Number.CalculationOperation add, Number.CalculationOperation substract, Number.CalculationOperation multiply, Number.CalculationOperation divide) {
+        super(add, substract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Number substract(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- eongun18_hausuebung2/src/main/java/htlgrieskirchen/pos/dreia/eongun18/HalloJavamitForEach.java
+++ eongun18_hausuebung2/src/main/java/htlgrieskirchen/pos/dreia/eongun18/HalloJavamitForEach.java
@@ -33,7 +33,7 @@ public class HalloJavamitForEach {
       
       list.forEach(System.out::print);
         
-    
+      
     }
     
 }
